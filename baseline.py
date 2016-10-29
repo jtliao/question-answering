f9685fd8e8e92067f4413e1219f6bfab92540b25
jtliao <jtliao9@yahoo.com>
2016-10-29 11:28:43 -0400

    started on baseline

---
+++ baseline.py
@@ -0,0 +1,19 @@
+import pickle
+
+
+# look for all the nouns in the question
+# look for sentences with the nouns,
+
+
+
+def get_nouns_from_questions(questions):
+    for num, question in questions.items():
+
+
+
+def main():
+    questions = pickle.load(open("num_to_question_dict.pkl", "rb"))
+
+
+if __name__ == "__main__":
+    main()
